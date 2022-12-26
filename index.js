8e8a23127a067c6b7c0d2be741d5fdff82bec4cf
guilhermehelon <guilhermehelton@gmail.com>
2022-12-26 16:08:34 -0300

    add: create-sheet

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { default: PlanilhaRepository } = require("./repository");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -6,6 +7,15 @@ app.get("/init", (req, res) => {
   return res.json({
     message: "ok",
   });
+});
+
+app.post("/create-sheet", (req, res) => {
+  try {
+    const sheet = new PlanilhaRepository();
+    sheet.desenhar(req.body);
+  } catch (error) {
+    console.log("Não foi possível criar");
+  }
 });
 
 app.listen(port, () => {
---
+++ repository.js
@@ -0,0 +1,50 @@
+const GoogleSpreadsheet = require("google-spreadsheet");
+const credentials = require("./credentials.json");
+
+export default class PlanilhaRepository {
+  static getDiasMes(month) {
+    month--;
+
+    const actualDate = new Date();
+    const year = actualDate.getFullYear();
+    const date = new Date(year, month, 1);
+    let days = [];
+    while (date.getMonth() === month) {
+      days.push(date.getDate());
+      date.setDate(date.getDate() + 1);
+    }
+    return days;
+  }
+
+  static diasFormatados(dias, mes) {
+    const actualDate = new Date();
+    const ano = actualDate.getFullYear();
+    const diasMes = [];
+    for (const dia of dias) {
+      const date = new Date(ano, mes, dia);
+      const index = date.getDay();
+
+      diasMes.push(`${DIAS_SEMANA[index]} ${dia}/${mes}/${ano}`);
+    }
+    return diasMes;
+  }
+
+  async desenhar(input) {
+    const doc = new GoogleSpreadsheet();
+    await doc.useServiceAccountAuth(credentials, "guilhermehelton@gmail.com");
+    await doc.createNewSpreadsheetDocument({ title: `${input.nomePlanilha}` });
+    const sheet1 = await doc.sheetsByIndex[0];
+    sheet1.updateProperties({ title: input.listaMembros[0] });
+    const diasDoMes = this.getDiasMes(input.mes);
+    const listaDiasFormatados = this.diasFormatados(diasDoMes, input.mes);
+    for (const membro of input.listaMembros) {
+      if (membro === input.listaMembros[0]) {
+      } else {
+        const newSpread = await doc.addSheet({ title: membro });
+        for (const i in diasDoMes) {
+          await newSpread.addRow(listaDiasFormatados);
+        }
+      }
+    }
+  }
+}
